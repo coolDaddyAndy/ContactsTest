94803337513d4e6853adccbb916ee751e7961298
Andrey Sushkov <>
2023-03-21 15:23:57 +0300

    Initial commit

---
+++ ContactsTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ContactsTest
+//
+//  Created by Andrei Sushkou on 21.03.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
